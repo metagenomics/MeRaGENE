397d5e5e578019fdc92d4fa22fdb61324889aec9
pbelmann <pbelmann@cebitec.uni-bielefeld.de>
2015-08-28 16:32:23 +0200

    create overview import fixed

--- test/test_createOverview.py
+++ test/test_createOverview.py
@@ -1,8 +1,8 @@
 __author__ = 'pbelmann'
 
 import nose.tools as nose
-from scripts import createOverview
+from scripts import create_overview
 
 def test_determine_class():
-    clazz = createOverview.determine_class("(Bla)CARB")
+    clazz = create_overview.determine_class("(Bla)CARB")
     nose.assert_equal(clazz,"A")
