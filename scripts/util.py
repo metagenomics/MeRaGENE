f3e758ef2d496836bd4b57918989972acc029eb8
pbelmann <pbelmann@cebitec.uni-bielefeld.de>
2015-08-30 16:16:09 +0200

    variables added

--- scripts/util.py
+++ scripts/util.py
@@ -1,2 +1,25 @@
 NOT_AVAILABLE = "N/A"
+
+# overview.txt identifier/header
 SUBJECT_ACCESSION = 'Subject accsession'
+SUBJECT_TITLES = 'Subject titles'
+SUBJECT_TAXIDS = "Subject tax ids"
+SUBJECT_IDS = "Subject ids"
+LINKS = "Links"
+GENE_SEQUENCE = "Gene sequence"
+IDENTITY = "Identity"
+EVALUE_BEST_BLASTP = "Evalue best blastp"
+BEST_BLASTP_HIT = "Best blastp hit"
+SCORE_HMM = "score HMM"
+CLASS = "Class"
+HMM = "HMM"
+EVAL_HMM = "Evalue HMM"
+GENE_ID = "Gene ID"
+OVERVIEW_TXT = "overview.txt"
+FAA_TXT_OUTPUT_FOLDER = "txt_faa_files"
+HTML_OUTPUT_FOLDER = "html_files"
+NO_LINK = "NO_LINK"
+
+# coverage.txt identifier
+CONTIG_NAME = "#ContigName"
+AVG_COVERAGE = "AvgCoverage"
